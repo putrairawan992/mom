f33940f3665ad80512c2169057e80673892ab3c5
panggih mario <panggihmario@gmail.com>
2019-06-21 10:25:33 +0700

    add feature upload and reafactor localization

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Upload as UploadAnt, Icon } from 'antd'
+import Button from '../Button'
+import './style.sass'
 
 const Upload = props => {
   const uploadButton = (
@@ -8,10 +10,29 @@ const Upload = props => {
     </div>
   )
 
+  const imageUpload = (
+    <div className="containerUpload">
+      <img src={props.imageUrl}  alt="avatar"/>
+      <Icon type="camera" className="cameraIcon"/>
+      <Icon type="delete" className="deleteIcon"/>
+      {
+        props.type === 'default' ?
+        <Button width="full" type="upload-default">Default</Button>:
+        <Button width="full" type="upload-non-default">Set Default</Button>
+      }
+      
+      
+    </div>
+  )
+
   return(
-    <UploadAnt {...props}>
-      {props.imageUrl ? <img src={props.imageUrl} width="200px" alt="avatar"/> : uploadButton}
-      {/* {uploadButton} */}
+    <UploadAnt 
+      {...props}
+      name="avatar"
+      listType="picture-card"
+      showUploadList={false}
+      >
+      {props.imageUrl ? imageUpload : uploadButton}
     </UploadAnt>
   )
 }
