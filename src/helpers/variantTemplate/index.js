f0ef1779e63d1644aeab6e3f57fb3b7bc6246b59
Candra Darmawan <chandra.souvenir@gmail.com>
2019-07-23 14:07:28 +0700

    fixed variant template

--- src/helpers/variantTemplate/index.js
+++ src/helpers/variantTemplate/index.js
@@ -1,5 +1,5 @@
 export const variantTemplate = (variants) => {
-    variants.map((variant, index, arr) => {
+    return variants.map((variant, index, arr) => {
     if (index % 2 === 0) return `${variant.value} : `;
     else {
       if (arr.length - 1 <= index) return `${variant.value}`;
