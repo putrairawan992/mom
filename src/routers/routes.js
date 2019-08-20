eb86f4785fb3ea5b0d4cf14a47651812dae9b103
Candra Darmawan <chandra.souvenir@gmail.com>
2019-08-20 10:57:52 +0700

    install yarn

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -3,18 +3,33 @@ import Login from "../pages/Login";
 import Home from "../pages/Home";
 import requiredAuth from '../hoc/requiredAuth';
 import notRequiredAuth from "../hoc/notRequiredAuth";
+import FullLayout from "../layouts/FullLayout";
+import MainLayout from "../layouts/MainLayout";
+import DashboardChina from "../pages/DashboardChina";
 //import MainLayout from "../layouts/MainLayout";
 
 const routes = [
   {
     path: PATH_URL.LOGIN,
     component: Login,
-    layoutName: 'fullLayout'
+    layout: FullLayout
   },
   {
     path: PATH_URL.HOME,
-    component: Home
-  }
+    component: Home,
+    layout: MainLayout
+  },
+  {
+    path: PATH_URL.DASHBOARD_CHINA,
+    component: DashboardChina,
+    layout: MainLayout
+  },
+  {
+    path: PATH_URL.DASHBOARD_INDONESIA,
+    component: DashboardChina,
+    layou: MainLayout
+  },
+
 ];
 
 export default routes;
